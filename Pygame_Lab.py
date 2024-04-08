022dd244d52b052ef3dbebf17f8c0aa1ef78fc95
H-Suen <hansel.lh.suen@gmail.com>
2024-04-08 15:40:40 -0400

    2024-04-08 3:40

--- Pygame_Lab.py
+++ Pygame_Lab.py
@@ -33,41 +33,53 @@ while running:
     # All game math and comparisons happen here
 
     # ----- DRAWING -----
-    screen.fill((223, 223, 0))  # always the first drawing command
+    screen.fill((0, 0, 0))  # always the first drawing command
 
     # Drawing Court
 
         # Border 
-    pygame.draw.rect(screen, (0, 0, 255), [0, 0, 700, 375], 5)
+    pygame.draw.rect(screen, (255, 0, 0), [0, 0, 700, 375], 5)
 
         # Middle and logo
-    pygame.draw.circle(screen, (255, 255, 255), [350, 188], 50)
-    pygame.draw.line(screen, (0, 0, 255), [350, 0], [350, 375], 5)
-    pygame.draw.arc(screen, (255, 0, 0), [300, 138, 100, 100], 3.57792, 2.26893, 7)
+    pygame.draw.line(screen, (255, 0, 0), [350, 0], [350, 375], 5)
+    pygame.draw.circle(screen, (0, 0, 0), [350, 188], 50)
+    pygame.draw.arc(screen, (255, 0, 0), [300, 138, 100, 100], 3.57792, 2.26893, 5)
     pygame.draw.polygon(screen, (255, 0, 0), [[319, 151], [353, 163], [363, 170], [369, 180], 
-    [370, 187], [365, 193], [355, 200], [347, 204], [340, 205], [346, 194], [330, 190], [320, 193], [312, 200]], 7)
-     
+    [370, 187], [365, 193], [355, 200], [347, 204], [340, 205], [346, 194], [330, 190], [320, 193], 
+    [312, 200], [306, 207]], 7)
+    pygame.draw.line(screen,(0, 0, 0), [307, 197], [319, 156], 9)
+    pygame.draw.arc(screen, (255, 0, 0), [344, 167, 10, 10], 2.87979, 6.02139, 75)
+
         # 3 point line (right side)
-    pygame.draw.line(screen, (0, 0, 255), [665, 27], [700, 27], 5)
-    pygame.draw.line(screen, (0, 0, 255), [665, 345], [700, 345], 7)
-    pygame.draw.arc(screen, (0, 0, 255), [525, 25, 325, 325], 1.5708, 4.71239, 5)
+    pygame.draw.line(screen, (255, 0, 0), [665, 27], [700, 27], 5)
+    pygame.draw.line(screen, (255, 0, 0), [665, 345], [700, 345], 7)
+    pygame.draw.arc(screen, (255, 0, 0), [525, 25, 325, 325], 1.5708, 4.71239, 5)
 
         # Free throw line (right side)
-    pygame.draw.circle(screen, (0, 0, 255), [575, 188], 50, 5)
-    pygame.draw.line(screen, (0, 0, 255), [575, 140], [700, 140], 5)
-    pygame.draw.line(screen, (0, 0, 255), [575, 235], [700, 235], 5)
-    pygame.draw.line(screen, (0, 0, 255), [575, 235], [575, 140], 5)
+    pygame.draw.circle(screen, (255, 0, 0), [575, 188], 50, 5)
+    pygame.draw.line(screen, (255, 0, 0), [575, 140], [700, 140], 5)
+    pygame.draw.line(screen, (255, 0, 0), [575, 235], [700, 235], 5)
+    pygame.draw.line(screen, (255, 0, 0), [575, 235], [575, 140], 5)
 
         # 3 point line (left side)
-    pygame.draw.line(screen, (0, 0, 255), [0, 27], [35, 27], 5)
-    pygame.draw.line(screen, (0, 0, 255), [0, 345], [35, 345], 7)
-    pygame.draw.arc(screen, (0, 0, 255), [-140, 25, 325, 325], 4.71239, 1.5708, 5)
+    pygame.draw.line(screen, (255, 0, 0), [0, 27], [35, 27], 5)
+    pygame.draw.line(screen, (255, 0, 0), [0, 345], [35, 345], 7)
+    pygame.draw.arc(screen, (255, 0, 0), [-140, 25, 325, 325], 4.71239, 1.5708, 5)
 
         # Free throw line (left side)
-    pygame.draw.circle(screen, (0, 0, 255), [132, 188], 50, 5)
-    pygame.draw.line(screen, (0, 0, 255), [0, 140], [125, 140], 5)
-    pygame.draw.line(screen, (0, 0, 255), [0, 235], [125, 235], 5)
-    pygame.draw.line(screen, (0, 0, 255), [133, 140], [133, 235], 5)
+    pygame.draw.circle(screen, (255, 0, 0), [132, 188], 50, 5)
+    pygame.draw.line(screen, (255, 0, 0), [0, 140], [125, 140], 5)
+    pygame.draw.line(screen, (255, 0, 0), [0, 235], [125, 235], 5)
+    pygame.draw.line(screen, (255, 0, 0), [133, 140], [133, 235], 5)
+
+        # Basketball ball
+    pygame.draw.circle(screen, (180, 86, 53), [225, 300], 25)
+    pygame.draw.line(screen, (0, 0, 0), [225, 275], [225, 325], 2)
+    pygame.draw.line(screen, (0, 0, 0), [200, 300], [250, 300], 2)
+    pygame.draw.arc(screen, (0, 0, 0), [235, 280, 34, 40], 2.18166, 4.10952, 2)
+    pygame.draw.arc(screen, (0, 0, 0), [183, 280, 34, 40], 5.22325, 1.059931, 2)
+
+
 
     # Must be the last two lines of the game loop
     pygame.display.flip()
